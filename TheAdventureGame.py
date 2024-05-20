a2e4cea610c656f8f9e4d7b7c13379b81e54bbb9
sangsioco <sangsioco@gmail.com>
2024-05-20 16:51:32 -0700

    adventure game task 1

---
+++ TheAdventureGame.py
@@ -0,0 +1,14 @@
+place = input("Choose a place: forest or cave? ")
+
+if place == "forest":
+    action = input("climb a tree or cross a river? ")
+    if action == "climb a tree":
+        print("You found a bird's nest!")
+    elif action == "cross a river":
+        print("You found a boat!")
+    else:
+        print("Invalid action!")
+elif place == "cave":
+    print("You find a hidden treasure!")
+else:
+    print("Invalid place!")
